c60d050b44e24b87a64f79cf2e35ccf4f9ee5ac9
Jorge C. Pereira <64277389+jp2435@users.noreply.github.com>
2022-10-17 13:32:13 +0100

    Four/Third completo

---
+++ four/third.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[101] = {};
+    bool Veri = true;
+    int inp, counter = 0;
+
+    do{
+        if(Veri){
+            cout << "Insere um nr positvo:";
+            cin >> inp;
+        }else{
+            break;
+        }
+        //cout << "DEV: counter = " << counter << endl;
+        if(counter >= 100 - 1){ // Numero do tamnho - 1(index começa em 0)
+            Veri = false;
+            cout << "Tamanho do Array completado" << endl;
+        }else if(inp <= 0){
+            Veri = false;
+        }
+
+        arr[counter] = inp;
+        counter++;
+    }while(Veri);
+
+
+    // cout << "DEV: counter(Final) = " << counter << endl;
+    int inpN, counterN = 0;
+    cout << "Enter a nr to see number of times:";
+    cin >> inpN;
+
+    for(int i = 0; i<counter;i++){
+        if(arr[i] == inpN){
+            counterN++;
+        }
+    }
+    cout << inpN << " apareceu " << counterN << " vezes" << endl;
+}
