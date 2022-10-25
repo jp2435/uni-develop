d8f809534b2d0243e2a2882d8a6d647d2ad98ca1
Jorge C. Pereira <64277389+jp2435@users.noreply.github.com>
2022-10-25 17:20:18 +0100

    Five/ex07 completo

--- five/ex07.cpp
+++ five/ex07.cpp
@@ -1,3 +1,25 @@
-//
-// Created by jorge on 25/10/2022.
-//
+#include <iostream>
+
+using namespace std;
+int ManyDigits(float number){
+    int counter = 0;
+    while(number>1){
+        number *= 0.1;
+        counter++;
+    }
+    return counter;
+}
+int main(){
+    int inp;
+    cout << "Go find how many digits your num use" << endl;
+    do{
+       cout << "Input:";
+       cin >> inp;
+       if(inp != 0){
+           break;
+       }
+    }while(true);
+
+    cout << "Your number use " << ManyDigits(inp) << endl;
+    return 0;
+}
